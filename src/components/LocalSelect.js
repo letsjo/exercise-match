f7e0555d5ca488d5e72d8b6e82c569fe30eb3c24
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-08-07 11:19:48 +0900

    first commit

---
+++ src/components/LocalSelect.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LocalSelect = () => {
+  return (
+    <div>LocalSelect</div>
+  )
+}
+
+export default LocalSelect
---
+++ src/components/SlideBanner.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Slider from "react-slick";
+
+const SlidBanner = ({show=3,page=3}) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: show,
+    slidesToScroll: page,
+  };
+  console.log(settings);
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+        <div>
+          <h3>9</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default SlidBanner;
