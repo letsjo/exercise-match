425ecff7c5f1a186f647934c75ae4337f03ff848
jjjjjwon <jjjjjwon21@gmail.com>
2022-08-07 22:31:02 +0900

    sweetalert2 setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import NavBar from "./components/NavBar";
+import Sweetalert from "./components/Sweetalert";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Sweetalert/>
     </div>
   );
 }
