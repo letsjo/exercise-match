773187ca83176b7508dce538c22abee8994474ff
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-08-12 02:21:56 +0900

    before merge

--- src/apis/userAPI.js
+++ src/apis/userAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const userAPI = axios.create({
-  baseURL: "http://13.125.106.21:8080",
+  baseURL: "http://13.125.210.74:8080",
   headers: { "Content-Type": "application/json" },
 });
 
