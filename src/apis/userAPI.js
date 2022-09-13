d48e566dcaf1220c197a0c8ed4bab98160181615
jjjjjwon <jjjjjwon21@gmail.com>
2022-09-13 22:01:04 +0900

    검색 매칭 진행중, 지역 전체 수정해야함

--- src/apis/userAPI.js
+++ src/apis/userAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const userAPI = axios.create({
-  baseURL: "http://3.34.190.24:8080",
+  baseURL: "http://15.165.56.192:8080",
   headers: { "Content-Type": "application/json" },
 });
 
