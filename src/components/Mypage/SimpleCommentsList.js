7e97037e120225af758867a49ffcd419976b0897
jjjjjwon <jjjjjwon21@gmail.com>
2022-09-14 13:57:33 +0900

    진행중

--- src/components/Mypage/SimpleCommentsList.js
+++ src/components/Mypage/SimpleCommentsList.js
@@ -5,11 +5,11 @@ import SimpleCard from "./SimpleCard";
 const SimpleCommentsList = () => {
   return (
     <Container>
-      <SimpleCard count={5} comment="친절하고 매너가 좋아요."/>
-      <SimpleCard count={10} comment="친절하고 매너가 좋아요."/>
-      <SimpleCard count={15} comment="친절하고 매너가 좋아요."/>
-      <SimpleCard count={20} comment="친절하고 매너가 좋아요."/>
-      <SimpleCard count={25} comment="친절하고 매너가 좋아요."/>
+      <SimpleCard count={2} comment="친절하고 매너가 좋아요."/>
+      <SimpleCard count={3} comment="응답이 빨라요."/>
+      <SimpleCard count={2} comment="세세하게 잘 가르쳐 줘요."/>
+      <SimpleCard count={1} comment="해당 운동에 대한 이해도가 높아요."/>
+      <SimpleCard count={2} comment="시간 약속을 잘지켜요."/>
     </Container>
   );
 };
